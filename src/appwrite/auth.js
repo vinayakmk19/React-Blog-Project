936826b2c2cb934737650e02d8e46eb2e9acbc7a
vinayakmk19 <vinayakmkgen@gmail.com>
2024-03-31 15:12:19 +0530

    created appwrite auth and database services

---
+++ src/appwrite/auth.js
@@ -0,0 +1,63 @@
+import config from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthServices {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+        console.log("Error in services :: createAccount() method", error)
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+        console.log("Error in services :: login() method", error)
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Error in services :: getCurrentUser() method", error);
+    }
+    return null;
+  }
+
+  async logout(){
+    try {
+        await this.account.deleteSessions();  
+    } catch (error) {
+        console.log("Error in services :: logout() method", error)
+    }
+  }
+}
+
+const authServices = new AuthServices();
+
+export default authServices;
---
+++ src/appwrite/databaseconfig.js
@@ -0,0 +1,118 @@
+import config from "../config/config";
+import { Client, Databases, Storage, Query } from "appwrite";
+
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        config.appwriteDatabaseId,
+        config.appwriteColletionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log("Error in Database services :: createPost() ", error);
+    }
+  }
+
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        config.appwriteDatabaseId,
+        config.appwriteColletionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log("Error in Database services :: updatePost() ", error);
+    }
+  }
+
+  async deletePost(slug){
+    try {
+        await this.databases.deleteDocument(config.appwriteDatabaseId, config.appwriteColletionId, slug);
+
+        
+    } catch (error) {
+        console.log("Error in Database services :: deletePost() ", error);
+        return false
+    }
+  }
+
+  async getPost(slug){
+    try {
+        return await this.databases.getDocument(config.appwriteDatabaseId, config.appwriteColletionId, slug)
+    } catch (error) {
+        console.log("Error in Database services :: getPost() ", error);
+        return false
+    }
+  }
+
+  async getPosts(queries = [Query.equal('status', 'active')]){
+    try {
+        return await this.databases.listDocuments(config.appwriteDatabaseId, config.appwriteColletionId, queries)
+        
+    } catch (error) {
+        console.log("Error in Database services :: getPosts() ", error);
+        return false 
+    }
+  }
+
+  // File Upload Services
+
+  async uploadFile(file){
+    try {
+        return await this.bucket.createFile( config.appwriteBucketId, ID.unique(), file)
+        
+    } catch (error) {
+        console.log("Error in Database services :: uploadFile() ", error);
+        return false 
+    }
+  }
+
+  async deleteFile(fileId){
+    try {
+        await this.bucket.deleteFile(config.appwriteBucketId, fileId);
+
+        return true;
+        
+    } catch (error) {
+        console.log("Error in Database services :: deleteFile() ", error);
+        return false    
+    }
+  }
+
+  getFilePreview(fileId){
+    return this.bucket.getFilePreview(config.appwriteBucketId, fileId)
+  }
+}
+
+
+
+const service = new Service();
+
+export default service;
