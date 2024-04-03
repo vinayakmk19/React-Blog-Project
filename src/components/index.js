46e46dc53cde71b86ef8c8cfa6ed9b23bba14047
vinayakmk19 <vinayakmkgen@gmail.com>
2024-04-03 17:58:09 +0530

    creating  pages

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,10 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import SignUp from "./SingUp";
+import Login from "./Login";
+import RTE from "./RTE";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
 
-export {Header, Footer, Container, Logo, LogoutBtn};
+export {Header, Footer, Container, Logo, LogoutBtn, RTE, SignUp, Login, PostForm, PostCard};
